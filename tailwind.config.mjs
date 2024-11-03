c6a3491469731beae045934dddb72c9ac3541ee9
lucas-barake <86905052+lucas-barake@users.noreply.github.com>
2024-11-03 18:28:01 -0500

    sticky headers

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,27 +1,25 @@
 // tailwind.config.mjs
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-  darkMode: 'class',
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  darkMode: "class",
   theme: {
     extend: {
       typography: {
         invert: {
           css: {
-            '--tw-prose-body': 'rgb(214, 219, 224)',
-            '--tw-prose-headings': 'rgb(255, 255, 255)',
-            '--tw-prose-links': 'rgb(59, 130, 246)',
-            '--tw-prose-bold': 'rgb(255, 255, 255)',
-            '--tw-prose-quotes': 'rgb(214, 219, 224)',
-            '--tw-prose-code': 'rgb(255, 255, 255)',
-            '--tw-prose-hr': 'rgb(55, 65, 81)',
-            '--tw-prose-th-borders': 'rgb(55, 65, 81)',
-          },
-        },
-      },
-    },
+            "--tw-prose-body": "rgb(229, 229, 229)",
+            "--tw-prose-headings": "rgb(255, 255, 255)",
+            "--tw-prose-links": "rgb(59, 130, 246)",
+            "--tw-prose-bold": "rgb(255, 255, 255)",
+            "--tw-prose-quotes": "rgb(214, 219, 224)",
+            "--tw-prose-code": "rgb(255, 255, 255)",
+            "--tw-prose-hr": "rgb(55, 65, 81)",
+            "--tw-prose-th-borders": "rgb(55, 65, 81)"
+          }
+        }
+      }
+    }
   },
-  plugins: [
-    require('@tailwindcss/typography'),
-  ],
-}
+  plugins: [require("@tailwindcss/typography")]
+};
